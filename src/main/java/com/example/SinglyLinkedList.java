1dd037c1cbee9636f90709ca2bd8420d4e3329be
Kaveesha Senarathne <kaveeshagimhan49@gmail.com>
2024-10-03 21:48:29 +0530

    create SinglyLinkedList.java and constructor

---
+++ src/main/java/com/example/SinglyLinkedList.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class SinglyLinkedList {
+    private Node head; // Reference to the head
+    private int size;
+
+    // Constructor to create empty list
+    public SinglyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+}
